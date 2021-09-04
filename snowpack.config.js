a9e4f50808a0bc1135e6858607aafca531dbfbd8
AnnatarHe <iamhele1994@gmail.com>
2021-09-05 01:44:21 +0800

    feat(build): add production build config

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,6 +1,8 @@
 // Snowpack Configuration File
 // See all supported options: https://www.snowpack.dev/reference/configuration
 
+const isProduction = process.env.NODE_ENV === 'production'
+
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
   root: 'src',
@@ -21,4 +23,9 @@ module.exports = {
     out: './dist'
     /* ... */
   },
+  optimize: {
+    bundle: isProduction,
+    minify: isProduction,
+    target: isProduction ? 'es2018' : 'es2020'
+  }
 };
