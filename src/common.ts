413bb30bab670922eaa2fd6a6a885fb52d278547
AnnatarHe <iamhele1994@gmail.com>
2021-09-14 23:18:41 +0800

    fix(style): fix logo url and style

--- src/common.ts
+++ src/common.ts
@@ -3,6 +3,6 @@ export const API_ENDPOINT = 'https://clippingkk-api.annatarhe.com/api/v2/graphql
 export const WEBSITE_ENDPOINT = 'https://clippingkk.annatarhe.com'
 // export const CDN_PREFIX = 'https://clippingkk-cdn.annatarhe.com'
 export const CDN_PREFIX = 'https://ck-cdn.annatarhe.cn'
-export const logoURL = 'https://clippingkk.annatarhe.com/logo-cb37656621e374fb394541bbd7425188.png'
+export const logoURL = CDN_PREFIX + '/ck-assets/logo.png'
 export const ROOT_SELECTOR_CLASS = 'ck-clipping-card'
 export const rootDOM = document.querySelector('.' + ROOT_SELECTOR_CLASS)!
